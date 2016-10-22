d7ee314ff3f6cf1e794948312c19932502bc64de
Dieu <timothee.legaldekerangal@epitech.eu>
2016-10-22 22:30:48 +0000

    ll

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var app = require('express')();
+var twitter = require('node-twitter');
 var bodyParser = require('body-parser');
-var Twitter = require('twitter-node-client').Twitter;
 var server = require('http').createServer(app);
 
+/*On read le fichier de configuration*/
+var cnf = '';
+var fs = require('fs');
+fs.readFile('config.txt', 'utf8', function(err, contents) {
+    console.log(contents);
+		cnf = contents.split(",");
+});
+console.log('after calling readFile');
+
+/* Les routes du server */
 app.get('/', function(req, res)
 	{
 	    res.render('client_menu.ejs');
@@ -16,17 +26,33 @@ app.get('/', function(req, res)
 	     res.render('client_hof.ejs');
 	 })
 
+/*etablissement des sockets et des données de communications*/
 var io = require('socket.io').listen(server);
-
 io.sockets.on('connection', function(socket)
 	      {
-                  socket.score = 0;
-		  socket.emit('score', 0);
-	      })
-    .sockets.on('twitt_ex', function(socket)
-		{
-		    socket.score++;
-		    socket.emit('score', socket.score);
-		});
+					socket.score = 0; //On set le score à 0
+					socket.emit('score', 0); //On transmet le score initial au client
+					socket.on('tweet_ex', function(socket) // Si un tweet a été explosé on incrémente le score
+	  			{
+	      		socket.score++;
+	      		socket.emit('score', socket.score);
+					});
+					/*On set l'API de stream twitter*/
+					/*var twitterStreamClient = new Twitter.StreamClient('CONSUMER_KEY','CONSUMER_SECRET','TOKEN','TOKEN_SECRET');
+					twitterStreamClient.on('close', function() {
+    				console.log('Connection closed.');
+					});
+					twitterStreamClient.on('end', function() {
+    				console.log('End of Line.');
+					});
+					twitterStreamClient.on('error', function(error) {
+    				console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+					});
+					twitterStreamClient.on('tweet', function(tweet) {
+						socket.emit('tweet', tweet);
+						console.log(tweet);
+					});
+					twitterStreamClient.start(cnf);*/
+				});
 
 server.listen(8080);
