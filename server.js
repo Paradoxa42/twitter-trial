5b5babc46bbe5c695949e6258160ef62a020ef81
Dieu <timothee.legaldekerangal@epitech.eu>
2016-10-23 22:27:45 +0000

    Communication socket.io fonctionnelle, pixi intégrée mais encore des problèmes avec l'API twitter nécéssite du temps supplémentaire

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var app = require('express')();
-var twitter = require('node-twitter');
+var Twitter = require('node-twitter');
 var bodyParser = require('body-parser');
 var server = require('http').createServer(app);
+var pixi = require('pixi')
 
 /*On read le fichier de configuration*/
 var cnf = '';
 var fs = require('fs');
 fs.readFile('config.txt', 'utf8', function(err, contents) {
-    console.log(contents);
 		cnf = contents.split(",");
 });
-console.log('after calling readFile');
 
+app.use(require('express').static('public'));
 /* Les routes du server */
 app.get('/', function(req, res)
 	{
@@ -31,14 +31,14 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket)
 	      {
 					socket.score = 0; //On set le score à 0
-					socket.emit('score', 0); //On transmet le score initial au client
-					socket.on('tweet_ex', function(socket) // Si un tweet a été explosé on incrémente le score
+					socket.emit('score', socket.score); //On transmet le score initial au client
+					socket.on('tweet_ex', function() // Si un tweet a été explosé on incrémente le score
 	  			{
 	      		socket.score++;
 	      		socket.emit('score', socket.score);
 					});
 					/*On set l'API de stream twitter*/
-					/*var twitterStreamClient = new Twitter.StreamClient('CONSUMER_KEY','CONSUMER_SECRET','TOKEN','TOKEN_SECRET');
+					var twitterStreamClient = new Twitter.StreamClient('FkdOiZc663EQtsBJB6FhGtgE5',' xPbDmVaWjRoBXUC8LpdWf1YIshxfXo0B6rWxvgAal13hI1Km3s','790315200557768704-vjcJmRa0nbOfFcyhKA2L72HqcThywUo',' gPaTWDhbPg5xnwNRw1W1r53NmCj11b7kXb5xPbOc0yxOR');
 					twitterStreamClient.on('close', function() {
     				console.log('Connection closed.');
 					});
@@ -49,10 +49,10 @@ io.sockets.on('connection', function(socket)
     				console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
 					});
 					twitterStreamClient.on('tweet', function(tweet) {
-						socket.emit('tweet', tweet);
+						//socket.emit('tweet', tweet);
 						console.log(tweet);
 					});
-					twitterStreamClient.start(cnf);*/
+					twitterStreamClient.start(cnf);
 				});
 
 server.listen(8080);
