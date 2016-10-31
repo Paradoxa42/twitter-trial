e39ffc9c39af520ddfab4fa9108a41484209d63e
Dieu <timothee.legaldekerangal@epitech.eu>
2016-10-31 20:45:20 +0000

    Changement d'API twitter, streaming implémenté, changement de fonctionnement de la lecture du fichier de configuration de asynchrone à synchrone

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var app = require('express')();
-var Twitter = require('node-twitter');
 var bodyParser = require('body-parser');
 var server = require('http').createServer(app);
 var pixi = require('pixi')
 
 /*On read le fichier de configuration*/
-var cnf = '';
 var fs = require('fs');
-fs.readFile('config.txt', 'utf8', function(err, contents) {
-		cnf = contents.replace('\n','');
-		console.log("config : " + cnf);
-});
+function getCnf(){
+	var cnf = fs.readFileSync('config.txt','utf8')
+	cnf = cnf.replace('\n','');
+	return cnf
+}
 
 app.use(require('express').static('public'));
 /* Les routes du server */
@@ -42,18 +41,16 @@ function formatTxt(str)
 		return str;
 }
 
-//Etablissement de L'API twitter
-//var twitterStreamClient = new Twitter.StreamClient('FkdOiZc663EQtsBJB6FhGtgE5','xPbDmVaWjRoBXUC8LpdWf1YIshxfXo0B6rWxvgAal13hI1Km3s','790315200557768704-n46IDXQuwkwV5y8TUzHiPdGfF3vjT7o','MRQISKaO9X8VHq2md9RKRJ8H86ZjlE6Et7BFA9ExWf7BD');
-var twitterSearchClient = new Twitter.SearchClient('FkdOiZc663EQtsBJB6FhGtgE5','xPbDmVaWjRoBXUC8LpdWf1YIshxfXo0B6rWxvgAal13hI1Km3s','790315200557768704-n46IDXQuwkwV5y8TUzHiPdGfF3vjT7o','MRQISKaO9X8VHq2md9RKRJ8H86ZjlE6Et7BFA9ExWf7BD');
-/*twitterStreamClient.on('close', function() {
-    console.log('Connection closed.');
-});
-twitterStreamClient.on('end', function() {
-    console.log('End of Line.');
-});
-twitterStreamClient.on('error', function(error) {
-    console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
-});
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         'FkdOiZc663EQtsBJB6FhGtgE5',
+  consumer_secret:      'xPbDmVaWjRoBXUC8LpdWf1YIshxfXo0B6rWxvgAal13hI1Km3s',
+  access_token:         '790315200557768704-n46IDXQuwkwV5y8TUzHiPdGfF3vjT7o',
+  access_token_secret:  'MRQISKaO9X8VHq2md9RKRJ8H86ZjlE6Et7BFA9ExWf7BD',
+})
+var stream = T.stream('statuses/filter', { track : [getCnf()]});
+console.log("configuration " + getCnf());
 /*etablissement des sockets et des données de communications*/
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket)
@@ -65,34 +62,18 @@ io.sockets.on('connection', function(socket)
 	      		socket.score++;
 	      		socket.emit('score', socket.score);
 					});
-					twitterSearchClient.search({'q': cnf}, function(error, result) {
-    					if (error)
-    					{
-        					console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
-    					}
-    					if (result)
-    					{
-								console.log('tweet get');
-								i = 0;
-								for (tmp in result.statuses)
-								{
-									if  (tmp == 0 && i == 1)
-									{
-										if (tmp == 0 && i == 0)
-											i = 1;
-									}
-									socket.emit('tweet', {'id' : result.statuses[tmp].id, 'userName' : result.statuses[tmp].user.name, 'screenName' : result.statuses[tmp].user.screen_name, 'pic' : result.statuses[tmp].user.profile_image_url, 'text' : formatTxt(result.statuses[tmp].text)});
-								}
-								console.log('tweet sent');
-    					}
-					});
-					//Quand un tweet est get on l'emet
-					/*twitterStreamClient.on('tweet', function(tweet) {
-										socket.emit('tweet', tweet);
-										//console.log(tweet);
-										console.log('tweet.');
-					});
-					twitterStreamClient.start([cnf]);*/
+					var weekAgo = new Date();
+					weekAgo.setDate(weekAgo.getDate() - 7);
+					//On fait d'abord une recherche des tweets de la semaine pour pas avoir un écran vide au début
+					T.get('search/tweets', { q: getCnf() + ' since:' + weekAgo.getUTCFullYear() + '-' + weekAgo.getUTCMonth() + '-' + weekAgo.getUTCDate(), count: 20 }, function(err, data, response) {
+  					for (tmp in data.statuses) {
+							socket.emit('tweet', {'id' : data.statuses[tmp].id, 'userName' : data.statuses[tmp].user.name, 'screenName' : data.statuses[tmp].user.screen_name, 'pic' : data.statuses[tmp].user.profile_image_url, 'text' : formatTxt(data.statuses[tmp].text)});
+						}
+					})
+					//on lance le stream et on get les tweets en temps réel et on l'envoi au client
+					stream.on('tweet', function(tweet){
+						socket.emit('tweet', {'id' : tweet.id, 'userName' : tweet.user.name, 'screenName' : tweet.user.screen_name, 'pic' : tweet.user.profile_image_url, 'text' : formatTxt(tweet.text)});
+					})
 					console.log("connection");
 				});
 server.listen(8080);
